a065eed8d57f6d7cd0f63f6c4dc5495cdfbdd9eb
PrithijitDey <101328438+PrithijitDey@users.noreply.github.com>
2022-04-07 17:10:14 +0530

    Material UI added for login page

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [],
   imports: [
     MatButtonModule,
     MatButtonToggleModule,
     CommonModule
+  ],
+  exports:[
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
+
 })
 export class MaterialModule { }
